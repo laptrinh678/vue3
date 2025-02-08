c61379e85a3930dc6ceb91b245adf0336c0ea8d9
lapvt <laptrinh678@gmail.com>
2025-02-08 17:51:35 +0700

    chuc nang component cha, component con, truyen du lieu giưa 2 component cha, con

--- src/components/Masonry.vue
+++ src/components/Masonry.vue
@@ -1,10 +1,12 @@
 <template>
 
-    <div class="items" >
-
+<input type="text" v-model="text">
+<button @click="sendDataToParentComponent()">Send data to Parent Component</button>    
+<button @click="changeTabFromComponentChild()">Change tab from component child</button>
+<div class="items" >
     <div class="item" v-for="item in hab" :key="item.key">
         <img :src="item.url" alt="" class="content">
-        <p class="action-gallery">kkk</p>
+        <p class="action-gallery">{{message}}</p>
     </div>
    
 
@@ -12,6 +14,9 @@
   </template>
 <script >
 export default {
+  props: {
+    message: String
+  },
   data() {
     return {
       hab: [
@@ -28,10 +33,20 @@ export default {
        { 'name':'lap1', 'url':'https://cdnphoto.dantri.com.vn/wY5GZKhuOQB283bqHCTQjiqRROc=/zoom/300_200/2025/02/05/vu-dang-minh-1738756664167.jpg' },
        {'name': 'l3', 'url': 'https://cdnphoto.dantri.com.vn/s8mbgenQOnl_RymHHGEIUORioLg=/thumb_w/480/thumb-video/short-video/2025/02/05/8568-1738744524328/0_00_00.jpg'},
        {'name': 'l4', 'url': 'https://cdnphoto.dantri.com.vn/ySXrsKbpP3oz1hMReEE0eN6GmvQ=/zoom/240_160/2024/04/22/bhyt-1713764279210.jpeg'}
-      ]
+      ],
+      text: null
     }
 
+  },
+  methods:{
+    sendDataToParentComponent(){
+      this.$emit('updateDataParent', this.text)
+    },
+    changeTabFromComponentChild(){
+      this.$emit('changeTabFromChild')
+    }
   }
+
 }
 
 </script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,24 +1,46 @@
-<script setup>
+<!-- <script setup>
 import TheWelcome from '../components/TheWelcome.vue'
 import Interpolation from '../components/Interpolation.vue'
 import TowWayBinding from '@/components/TowWayBinding.vue'
-import Masony from '../components/Masony.vue'
-</script>
+import Masonry from '../components/Masonry.vue'
+</script> -->
 
-<template>
-  <main>
-    <p>kkk</p>
-    <TheWelcome />
-    <Interpolation/>
-    <TowWayBinding/>
-    <Masony />
-  </main>
+  <template>
+  <el-tabs v-model="activeName" class="demo-tabs" @tab-click="handleClick">
+    <el-tab-pane label="Masonry" name="first">Masonry
+      <p>Show data send from component Child: {{ message }}</p>
+      <Masonry :message="message" @updateDataParent="showDataChild" @changeTabFromChild="changeTab"/>
+    </el-tab-pane>
+    <el-tab-pane label="Config" name="second">Config</el-tab-pane>
+    <el-tab-pane label="Role" name="third">Role</el-tab-pane>
+    <el-tab-pane label="Task" name="fourth">Task</el-tab-pane>
+  </el-tabs>
 </template>
 
-<script>
-export default {
-  data() {
 
-  }
+<script lang="ts" setup>
+import {ref } from 'vue'
+import type { TabsPaneContext } from 'element-plus'
+import Masonry from '../components/Masonry.vue'
+const activeName = ref('first')
+const handleClick = () => {
+  console.log(activeName.value);
+}
+const message = ref('kkkk');
+const showDataChild = (dataFromComponentChild)=>{
+console.log(dataFromComponentChild)
+message.value =   dataFromComponentChild
+
+}
+const changeTab = ()=>{
+  activeName.value = 'second'
 }
 </script>
+<style>
+.demo-tabs > .el-tabs__content {
+  padding: 32px;
+  color: #6b778c;
+  font-size: 32px;
+  font-weight: 600;
+}
+</style>

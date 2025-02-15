908eb963af2c7bcd6040c3f4fc41ced145ff0b65
lapvt <laptrinh678@gmail.com>
2025-02-15 17:22:29 +0700

    truyen du lieu tu component cha va nhan tại component con

---
+++ src/components/parent-child-component/Child.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <p>Đây là component con</p>
+        <p>Data fix cứng: {{ message }} --thì property ở component cha không cần có dấu : ở trước (message="hab555") </p>
+        <p>Data động : {{ dataInReturn }} -- thì property ở component cha cần có dấu : ở trước (:dataInReturn="nameUser")</p>
+        <p>Nhận dữ liệu từ method ở component cha: {{ userAge }}</p>
+    </div>
+</template>
+<script>
+export default {
+    // cần khai báo các props ở đây, và định kiểu dữ liệu thì mới hiển thị được
+    props: {
+        message: String,
+        dataInReturn: String,
+        userAge: String
+    }
+}
+</script>
---
+++ src/components/parent-child-component/Parent.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+         <h1>Truyền dữ liệu từ component cha sang con - đây là component cha</h1>
+         <p>B1: khai báo component chile và component parent </p>
+         <p>B2: Import component Child vào component parent </p>
+         <p>B3: Khai báo tên thuộc tính trên component cha</p>
+         <Child message="hab555"  :dataInReturn="nameUser" :userAge="setAge()"/>
+    </div>
+</template>
+<script>
+import Child from './Child.vue';
+export default {
+    components: {
+        Child
+    },
+    data () {
+        return {
+            nameUser : 'Nguyễn văn ABC',
+            age: 15
+        }
+        
+    },
+    methods: {
+        setAge(){
+            return 155;
+        }
+    }
+}
+</script>
